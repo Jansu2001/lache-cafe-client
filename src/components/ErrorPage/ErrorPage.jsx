266aaa20b99519cd34ccbd72f6cacf526fa19e99
jansu2001 <sadnansujan23@gmail.com>
2023-05-04 16:05:22 +0600

    error page setUp

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import { FaceFrownIcon } from '@heroicons/react/24/solid'
+import logo from '../../assets/error.jpg'
 import React from 'react'
 import { Link, useRouteError } from 'react-router-dom'
 
@@ -7,7 +8,8 @@ const ErrorPage = () => {
     return (
         <section className="d-flex align-items-center vh-100 p-4 bg-light text-dark">
             <div className="container d-flex flex-column justify-content-center align-items-center px-5 my-8">
-                <FaceFrownIcon className="w-25 h-25 text-warning" />
+                {/* <FaceFrownIcon className="w-25 h-25 text-warning" /> */}
+                <img className="w-25 h-25 text-warning" src={logo} alt="" />
                 <div className="mx-auto text-center">
                     <h2 className="mb-8 font-weight-bold display-1 text-warning">
                         <span className='sr-only'>Error</span>
